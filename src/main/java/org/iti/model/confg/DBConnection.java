99b306ce86a7a1cd8454fc5784c1492194c18dbd
pc <mariamragab.ieee@gmail.com>
2020-04-10 16:41:11 -0700

    SessionFactorySingletone

--- src/main/java/org/iti/model/confg/DBConnection.java
+++ src/main/java/org/iti/model/confg/DBConnection.java
@@ -1,4 +1,21 @@
 package org.iti.model.confg;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class DBConnection {
+
+
+    private static SessionFactory sessionFactory = null;
+
+    private DBConnection() {
+    }
+
+    public static synchronized SessionFactory getInstance() {
+        if (sessionFactory == null || sessionFactory.isClosed()) {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
