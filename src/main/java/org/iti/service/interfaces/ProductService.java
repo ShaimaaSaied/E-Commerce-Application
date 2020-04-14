0774f0a911e01fd686fa7d6e3d167afea698810e
DELL <radwahussien24@gmail.com>
2020-04-14 21:42:39 +0200

    add Productinterface and ProductImpl

--- src/main/java/org/iti/service/interfaces/ProductService.java
+++ src/main/java/org/iti/service/interfaces/ProductService.java
@@ -1,4 +1,25 @@
 package org.iti.service.interfaces;
 
+import org.iti.model.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> selectAllProduct();
+
+    Product selectProductById(int id);
+
+    List<Product> selectProductByName(String name);
+
+    List<Product> selectProductByPrice(double price);
+
+    void addProduct(Product product);
+
+    boolean deleteProduct(int id);
+
+    boolean updateProductPrice(int id, double price);
+
+    boolean updateProductStock(int id, int stock);
+
 }
