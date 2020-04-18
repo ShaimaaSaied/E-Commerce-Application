54309bb89eea33cd48d3ff39d162e0d892c4aa06
DELL <radwahussien24@gmail.com>
2020-04-19 00:17:39 +0200

    view and edit Profile

--- src/main/java/org/iti/controller/user/Signin.java
+++ src/main/java/org/iti/controller/user/Signin.java
@@ -5,13 +5,19 @@ import org.iti.service.impl.UserServiceImpl;
 import org.iti.service.interfaces.UserService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name="login", urlPatterns = "/login")
 public class Signin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("signin_signup/html/SigninSignUp.html").include(req, resp);
 
+    }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -19,11 +25,14 @@ public class Signin extends HttpServlet {
         String password = req.getParameter("password");
         System.out.println("password " + password);
         System.out.println("email " + email);
+
         UserService userService = new UserServiceImpl();
         User user = userService.retriveUser(email, password);
         System.out.println("User : inside singin " + user);
         if (user != null) {
-            resp.sendRedirect("user/home/home.html");
+            req.getSession().setAttribute("currentuser",user);
+            resp.sendRedirect("/ECommerce/home");
+
         } else {
             req.getRequestDispatcher("signin_signup/html/SigninSignUp.html").forward(req, resp);
         }
