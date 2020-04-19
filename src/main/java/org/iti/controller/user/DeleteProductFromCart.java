e5454b72295236ca51f95ed3f7fa9d401a893560
SARA <sara.ayman256@gmail.com>
2020-04-19 10:24:51 +0200

    implemntion of delte product

--- src/main/java/org/iti/controller/user/DeleteProductFromCart.java
+++ src/main/java/org/iti/controller/user/DeleteProductFromCart.java
@@ -1,4 +1,33 @@
 package org.iti.controller.user;
 
-public class DeleteProductFromCart {
+import org.iti.model.dao.daoimpl.CartDaoImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/DeleteCart")
+public class DeleteProductFromCart extends HttpServlet {
+    CartDaoImpl cartDao;
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        cartDao = new CartDaoImpl();
+        int id = Integer.parseInt(request.getParameter("id"));
+        int productId = Integer.parseInt(request.getParameter("productId"));
+        boolean addCart = cartDao.removeProductFromCart(id, productId);
+
+        String nextJSP = "/CartHandlerServlet";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+        dispatcher.include(request, response);
+    }
+
+
 }
+
