feae6bca7227f592e16934e48dd77d66469f9d33
SARA <sara.ayman256@gmail.com>
2020-04-20 16:19:39 +0200

    implement logout for admin

---
+++ src/main/java/org/iti/controller/admin/Logout.java
@@ -0,0 +1,25 @@
+package org.iti.controller.admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "logout", urlPatterns = {"/admin/logout"})
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getSession(false).invalidate();
+        response.sendRedirect("SigninSignUp.html");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
